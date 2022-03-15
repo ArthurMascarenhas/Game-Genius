396ece97f5096106f3506b4baf7d4ae303deec16
ArthurMascarenhas <arthurcmascarenhas@gmail.com>
2022-03-15 09:23:16 -0300

    Update Javascript
    Atualizando o Javascript

--- script.js
+++ script.js
@@ -19,6 +19,17 @@ let shuffleOrder = () => {
     clickedOrder = [];
 
     for(let i in order) {
-        let elementColor 
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i)+ 1)
     }
+}
+
+let lightColor = (element, number) => {
+number = number * 500;
+setTimeout(()=>{
+element.classList.add('selected');
+}, number -250)
+setTimeout(() => {
+element;classlist.remove('selected')
+})
 }
